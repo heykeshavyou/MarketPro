c1bc41b5f00408ce379b159ef7e12ef698a6a832
Keshav <keshav210305@gmail.com>
2025-08-18 23:32:34 +0530

    changes

--- src/app/blog/blog.ts
+++ src/app/blog/blog.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { BreadCrumb } from '../Components/bread-crumb/bread-crumb';
+import { Info } from "../Components/info/info";
+import { ProductService } from '../Services/product-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
-  imports: [BreadCrumb],
+  imports: [BreadCrumb, Info, RouterLink],
   templateUrl: './blog.html',
   styleUrl: './blog.css',
 })
 export class Blog {
+
+  constructor(public productService:ProductService) {
+    
+  }
   Posts = [
     {
       title: 'Once determined you need to come up with a name',
